257e7e82bdcbb69b9a7ff4c4eedb98f945e1240c
SBT-Shutov-LL <llshutov.sbt@sberbank.ru>
2017-12-29 10:51:46 +0300

    add saving and restoring lots of result values in map

--- src/main/java/ru/mtuci/shutov/leonid/server/controllers/MainController.java
+++ src/main/java/ru/mtuci/shutov/leonid/server/controllers/MainController.java
@@ -1,7 +1,12 @@
 package ru.mtuci.shutov.leonid.server.controllers;
 
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,16 +14,26 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MainController {
-	private String data;
+	private static final String DELIMITER = " ";
+	private Map<String, String> data = new HashMap<>();
 
-	@RequestMapping(value = "/data", method = RequestMethod.GET)
-	public String getData() {
-		return data;
+	@RequestMapping(value = "/data/{id}", method = RequestMethod.GET)
+	public String getData(@PathVariable String id) {
+		return data.get(id);
 	}
 
-	@RequestMapping(value = "/data", method = RequestMethod.POST)
-	public ResponseEntity<String> setData(@RequestBody String data) {
-		this.data = data;
-		return new ResponseEntity<>(data, HttpStatus.OK);
+	@RequestMapping(value = "/data", method = RequestMethod.GET)
+	public String getAllData() {
+		String res = "";
+		for (Entry<String, String> entry : data.entrySet()) {
+			res = res.concat(DELIMITER).concat(entry.getKey()).concat(DELIMITER).concat(entry.getValue());
+		}
+		return res;
+	}
+
+	@RequestMapping(value = "/data/{id}", method = RequestMethod.POST)
+	public ResponseEntity<String> setData(@PathVariable String id, @RequestBody String data) {
+		this.data.put(id, data);
+		return new ResponseEntity<>(this.data.get(id), HttpStatus.OK);
 	}
 }
